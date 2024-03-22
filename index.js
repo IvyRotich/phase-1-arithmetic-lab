91ce0fc962deb4a7a847ac021c06d3a61d96de47
Ivy Rotich <ivytetich@gmail.com>
2024-03-22 13:12:10 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1=31;
+let num2=2;
 
-// Write your code here
+
+let multiply = num1*num2;
+
+
+let random = Math.ceil(Math.random()*10)
+
+let num3 = 12
+let num4 = 8
+let mod = num3 % num4
+
+
+let max = Math.max(10,15,20)
